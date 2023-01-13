c6d77d0e181c252e2dcf34dd496e737ce5e8c3c9
717wolf147 <122560706+717wolf147@users.noreply.github.com>
2023-01-13 13:13:15 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.QuarterNote)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.EigthNote)
+})
+basic.showIcon(IconNames.Skull)
