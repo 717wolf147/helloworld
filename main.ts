abe81daa7ff672b94a6a10dce06b5bba48b89d86
717wolf147 <122560706+717wolf147@users.noreply.github.com>
2023-01-13 13:28:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.QuarterNote)
+    music.playMelody("D E B C A G E C5 ", 221)
 })
 input.onButtonPressed(Button.B, function () {
+    music.playTone(587, music.beat(BeatFraction.Breve))
     basic.showIcon(IconNames.EigthNote)
 })
 basic.showIcon(IconNames.Skull)
+music.playMelody("G E A F G A B C5 ", 120)
